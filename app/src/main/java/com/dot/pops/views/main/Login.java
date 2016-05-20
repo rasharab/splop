c2dd4a9e1c7a571f9d927665fd043089749ec72d
bekti <bektic.22@gmail.com>
2016-05-20 13:38:56 +0700

    layout_login without facebook

---
+++ app/src/main/java/com/dot/pops/views/main/Login.java
@@ -0,0 +1,36 @@
+package com.dot.Pops.views.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.dot.Pops.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import butterknife.Bind;
+
+/**
+ * Created by Win7 on 20/05/2016.
+ */
+
+public class Login extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.login);
+
+    }
+}
--- app/src/main/java/com/dot/pops/views/main/SplashScreen.java
+++ app/src/main/java/com/dot/pops/views/main/SplashScreen.java
@@ -1,9 +1,15 @@
 package com.dot.Pops.views.main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.dot.Pops.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -11,5 +17,12 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.login)
+    protected void toLogin(){
+        Intent i = new Intent(SplashScreen.this, Login.class);
+        startActivity(i);
     }
 }
